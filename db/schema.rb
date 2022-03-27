0ab213fe5f657e4309c7888297c617c4e3af33bd
Soumik Maity <soumikmaity605@gmail.com>
2022-03-27 18:53:44 +0530

    initial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_122519) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_27_131337) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_122519) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
